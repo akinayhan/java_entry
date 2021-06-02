9c36e4d127be412bd97ec160f68c08361fb08671
akın ayhan <akinayhan@hotmail.com.tr>
2021-06-02 20:30:11 +0300

    02.06.

--- src/DbHelper.java
+++ src/DbHelper.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 public class DbHelper {
     private String userName="root";
-    private String password="___";
+    private String password="__";
     private String dbUrl="jdbc:mysql://localhost:3306/world?useSSL=false&serverTimezone=UTC";
     
     public Connection getConnection()throws SQLException{
